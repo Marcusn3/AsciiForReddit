e70bf8742019321e1d4b29f7c407fe001101f746
Marcusn3 <marcus@mniermann.de>
2019-10-25 07:04:43 +0200

    Cleaned up Code

--- reddit.py
+++ reddit.py
@@ -1,8 +1,6 @@
-from time import sleep
-from os import system
-import praw
-import logging
 import ascii
+import praw
+
 
 reddit = praw.Reddit("bot1")
 auth_status = str(reddit.read_only)
@@ -11,6 +9,8 @@ EXTENSIONS = [".jpg", ".jpeg", ".png"]
 subreddit = reddit.subreddit(
     input("Which Subreddit should be Scraped? (without /r/)\n")
 )
+
+
 postlimit = int(input("Number of Posts to Scrape?\n"))
 print(divider)
 for submission in subreddit.hot(limit=postlimit):
@@ -25,11 +25,3 @@ for submission in subreddit.hot(limit=postlimit):
     output = ascii.loadFromUrl(url)
     print("by u/" + author)
     print(divider)
-
-
-# for submission in reddit.subreddit('all').top('all'):
-#    print(submission.title)
-# submission.reply(reply_text)
-#
-# for submission in subreddit.stream.submissions():
-#     do something with submission
